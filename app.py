e02fc95d1da35941e218146aba29835aa8009d06
BayuDC <dev.bayudc@gmail.com>
2022-06-05 11:53:23 +0700

    Define the routes

--- app.py
+++ app.py
@@ -1,7 +1,20 @@
-from flask import Flask, render_template
+from crypt import methods
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/', methods=['post'])
+def store_url():
+    url = request.json['url']
+    return "Shorten the url to"
+
+
+@app.route('/<token>')
+def show_url(token):
+    return 'Redirect to...'
